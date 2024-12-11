54e0859275f4d0f1636170923cc7a7fc9cd93052
mattg07 <matiasmgodoy00@gmail.com>
2024-12-12 00:21:01 +0100

    ui part two

--- src/components/FilterSideBar.tsx
+++ src/components/FilterSideBar.tsx
@@ -1,7 +1,20 @@
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
+async function filterJobs(formData: FormData){
+"use server"
+console.log(formData)
+}
+
 export default function FilterSideBar() {
   return (
-    <aside className="sticky top-0 h-fit rounded-lg border bg-red-600 md:w-[260px]">
-      hola
+    <aside className="sticky top-0 p-4 h-fit rounded-lg border bg-gray-50 md:w-[260px]">
+        <form action={filterJobs}>
+            <div className="flex flex-col gap space-y-4">
+               <Label htmlFor="q">Search</Label> 
+               <Input id="q" name="q" placeholder="Title, company, etc"/>
+            </div>
+        </form>
     </aside>
   );
 }
