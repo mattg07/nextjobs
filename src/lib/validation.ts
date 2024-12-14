d288c7a5507cc8a5dfd1e4e86bbe2ca85bb24dd4
mattg07 <matiasmgodoy00@gmail.com>
2024-12-14 22:59:40 +0100

    zod an ui done

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,28 +1,54 @@
 import { z } from "zod";
-import { jobTypes } from "./JobsTypes";
+import { jobTypes, locationType } from "./JobsTypes";
 
 const requiredString = z.string().min(1, "Required");
-const numericRequiredString = requiredString.regex(/^\d+$/, "Must be a number")
+const numericRequiredString = requiredString.regex(/^\d+$/, "Must be a number");
 const companyLogoSchema = z
   .custom<File | undefined>()
-  .refine((file) => {
-    return !file || (file instanceof File && file.type.startsWith("image/"));
-  }, "Must be image file")
+  .refine((file) => 
+ !file || (file instanceof File && file.type.startsWith("image/")), "Must be image file")
   .refine((file) => {
     return !file || file.size < 1024 * 1024 * 2;
   }, "File must  be less than 2MN");
 
-export const createJobSchema = z.object({
-  title: requiredString.max(100),
-  type: requiredString.refine(
-    (value) => jobTypes.includes(value),
-    "Invalid Job Type",
+const applicationSchema = z
+  .object({
+    applicationEmail: z.string().max(100).email().optional().or(z.literal("")),
+    applicationUrl: z.string().max(100).url().optional().or(z.literal("")),
+  })
+  .refine((data) => data.applicationEmail || data.applicationUrl, {
+    message: "Email or url is required",
+    path: ["applicationEmail"],
+  });
+
+const locationSchema = z.object({
+  locationType: requiredString.refine(
+    value => locationType.includes(value), "Invalid Location"
   ),
-  companyName: requiredString.max(100),
-  companyLogo: companyLogoSchema,
-  description: z.string().max(1200).optional(),
-  salary: numericRequiredString.max(9,"Number cant be longer that 9 digits")
-});
+  location : z.string().max(100).optional()
+}).refine(
+  data => !data.locationType || data.locationType === "Remote" || data.location, {
+    message: "Location is required for onsite jobs",
+    path: ["location"]
+  }
+)
+
+export const createJobSchema = z
+  .object({
+    title: requiredString.max(100),
+    type: requiredString.refine(
+      (value) => jobTypes.includes(value),
+      "Invalid Job Type",
+    ),
+    companyName: requiredString.max(100),
+    companyLogo: companyLogoSchema,
+    description: z.string().max(1200).optional(),
+    salary: numericRequiredString.max(9, "Number cant be longer that 9 digits"),
+  })
+  .and(applicationSchema)
+  .and(locationSchema )
+
+  export type CreateJobValues = z.infer<typeof createJobSchema>
 
 export const jobFilterSchema = z.object({
   q: z.string().optional(),
