27ead901027a869dfbd6b4c526ccb78120e76ce1
mattg07 <matiasmgodoy00@gmail.com>
2024-12-17 01:01:18 +0100

    server actions to submit form

--- src/app/jobs/new/actions.ts
+++ src/app/jobs/new/actions.ts
@@ -6,6 +6,7 @@ import { nanoid } from "nanoid";
 import {put} from "@vercel/blob"
 import path from "path";
 import prisma from "@/lib/prisma"
+import { redirect } from "next/navigation";
 
 
 
@@ -58,5 +59,5 @@ export async function createJobPosting(formData: FormData) {
         }
 
     })
-
+    redirect("/job-submitted")
 }
