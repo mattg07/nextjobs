5000cf84ca14c0e0a69cb7a704762c3a7b51c0bc
mattg07 <matiasmgodoy00@gmail.com>
2024-12-18 15:18:59 +0100

    metadata for single job display

--- src/app/jobs/[slug]/page.tsx
+++ src/app/jobs/[slug]/page.tsx
@@ -2,31 +2,53 @@ import prisma from "@/lib/prisma";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
-import { CalendarIcon, MapPinIcon, Building2Icon, BanknoteIcon } from 'lucide-react';
+import {
+  CalendarIcon,
+  MapPinIcon,
+  Building2Icon,
+  BanknoteIcon,
+} from "lucide-react";
+import { cache } from "react";
+import { Metadata } from "next";
 
-export default async function Page(props: { params: Promise<{ slug: string }> }) {
-  const params = await props.params;
-  const slug = await params?.slug;
+interface JobPageProps {
+  params: { slug: string };
+}
 
-  if (typeof slug !== "string") {
-    notFound();
-  }
-
+const getJob = cache(async (slug: string) => {
   const job = await prisma.job.findUnique({
-    where: {
-      slug: slug,
-    },
+    where: { slug },
   });
+  if (!job) notFound();
+  return job;
+});
 
-  if (!job) {
-    notFound();
-  }
+export async function generateMetadata({
+  params,
+}: JobPageProps): Promise<Metadata> {
+  const {slug} = await params;
+  const job = await getJob(slug);
+  return {
+    title: job.title,
+  };
+}
 
+
+
+
+export default async function Page({ params }: JobPageProps) {
+  const { slug } = await params;
+  const job = await getJob(slug);
   return (
     <main className="container mx-auto my-8 px-4">
-      <Card className="max-w-4xl mx-auto">
+      <Card className="mx-auto max-w-4xl">
         <CardHeader className="space-y-4">
           <div className="flex items-center space-x-4">
             <Image
@@ -64,15 +86,19 @@ export default async function Page(props: { params: Promise<{ slug: string }> })
         </CardHeader>
         <CardContent className="space-y-6">
           <section>
-            <h2 className="text-2xl font-semibold mb-2">Job Description</h2>
-            <div className="prose max-w-none" dangerouslySetInnerHTML={{ __html: job.description }} />
+            <h2 className="mb-2 text-2xl font-semibold">Job Description</h2>
+            <div
+              className="prose max-w-none"
+              dangerouslySetInnerHTML={{ __html: job.description }}
+            />
           </section>
         </CardContent>
         <CardFooter className="border-red-900">
-          <Button size="lg" className="m-auto">Apply Now</Button>
+          <Button size="lg" className="m-auto">
+            Apply Now
+          </Button>
         </CardFooter>
       </Card>
     </main>
   );
 }
-
